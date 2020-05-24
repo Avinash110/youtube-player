7df3757004f712e0ac48bc406aaccf547ea48a49
Avinash110 <maheshwariavinash50@gmail.com>
2020-05-23 19:56:02 -0700

    play pause icon updated

--- components/VideoPlayer/VideoPlayer.js
+++ components/VideoPlayer/VideoPlayer.js
@@ -64,10 +64,11 @@ export default class VideoPlayer extends React.Component {
                     <div className="progress-bar"></div>
                     <div className="controls">
                         <button onClick={this.onPlay} className="play">
-                            <svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%">
+                            {!this.state.isVideoPlaying ? <svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%">
                                 <use className="shadow"></use>
-                                <path className="play-path" d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z" id="ytp-id-56"></path>
-                            </svg>
+                                <path className="controls-play-path" d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z" id="ytp-id-56"></path>
+                            </svg> : <svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use className="ytp-svg-shadow"></use><path className="controls-play-path" d="M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z" id="ytp-id-91"></path></svg>
+                        }
                         </button>
                     </div>
                 </div>
